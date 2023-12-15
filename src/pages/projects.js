e58675b2bf0ed3d9dbc8780008e779bb52a3db0b
kapil sharma <kapil.sharma@intimetec.com>
2023-12-15 21:44:02 +0530

    completed articles section

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,6 +6,9 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { GithubIcon } from '@/component/Icons'
 import projectImage1 from '../../public/images/projects/crypto-screener-cover-image.jpg'
+import {motion} from "framer-motion"
+
+const FramerImage = motion(Image);
 
 const FeaturedProject = ({type, title, summary, img, link, github }) =>{
     return (
@@ -14,7 +17,9 @@ const FeaturedProject = ({type, title, summary, img, link, github }) =>{
             <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark 
             rounded-br-3xl'/>
             <Link href={link} target='_black' className='w-1/2 cursor-pointer overflow-hidden rounded-lg'>
-                <Image src={img} alt={title} className="w-full h-auto" />
+                <FramerImage src={img} alt={title} className="w-full h-auto" 
+                    whileHover={{scale:1.05}}
+                    transition={{duration:0.2}}/>
             </Link>
             <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
                 <span className='text-primary font-medium text-xl'>{type}</span>
@@ -42,7 +47,9 @@ const Project = ({type, title, img, link, github }) =>{
         <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark 
             rounded-br-3xl'/>
             <Link href={link} target='_black' className='w-full cursor-pointer overflow-hidden rounded-lg'>
-                <Image src={img} alt={title} className="w-full h-auto" />
+                <FramerImage src={img} alt={title} className="w-full h-auto" 
+                whileHover={{scale:1.05}}
+                transition={{duration:0.2}} />
             </Link>
             <div className='w-full flex flex-col items-start justify-between mt-4'>
                 <span className='text-primary font-medium text-xl'>{type}</span>
